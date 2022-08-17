26d408d9c44e9dc8a71dc5649a7669c62e495f3b
sdavitt <sdavt2@gmail.com>
2022-08-17 16:37:27 -0500

    POST route workshop

--- app/models.py
+++ app/models.py
@@ -13,14 +13,20 @@ class Player(db.Model):
     injured = db.Column(db.Boolean)
     suspended = db.Column(db.Boolean)
 
-    def __init__(self, fname, lname, team='Manchester City', pos='Midfield', inj=False, sus=False):
-        self.id = uuid4()
-        self.first_name = fname
-        self.last_name = lname
-        self.team = team
-        self.position = pos
-        self.injured = inj
-        self.suspended = sus
+    def __init__(self, a_dict):
+        self.id = str(uuid4())
+        self.first_name = a_dict['first_name']
+        self.last_name = a_dict['last_name']
+        self.team = a_dict['team']
+        self.position = a_dict['pos']
+        if a_dict.get('injured'): # if there is an injured key in the dict
+            self.injured = a_dict['injured']
+        else:
+            self.injured = False
+        if a_dict.get('suspended'): # if there is an suspended key in the dict
+            self.suspended = a_dict['suspended']
+        else:
+            self.suspended = False
 
 
     def to_dict(self):
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,52 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import app
 from app.models import db, Player
 
 
 @app.route('/players')
 def getPlayers():
+    """ [GET] all player information """
     return jsonify({'Players': [p.to_dict() for p in Player.query.all()]}), 200
+
+# purpose of our POST request - to allow a user/dev/some code source to send us player data
+# we need to specify how this data is organized/what data we are getting
+# my POST request will accept a dictionary containing
+    # REQUIRED - all information that is non-nullable about my object
+    # OPTIONAL - all optional attributes of my object
+@app.route('/newplayer', methods=['POST'])
+def createPlayer():
+    """
+    [POST] Create a new player in my database
+    Accepts JSON data in the following format:
+    {
+        first_name: <STRING>,
+        last_name: <STRING>,
+        team: <STRING>,
+        pos: <STRING> Midfielder | Defender | Keeper | Striker,
+        injured: <OPTIONAL BOOLEAN>,
+        suspended: <OPTIONAL BOOLEAN>
+    }
+    """
+    try:
+        # we need to access the payload/data from the request
+        player_dict = request.get_json() # Python dictionary
+        # then we need to transform it into a Player object
+        new_player = Player(player_dict)
+    except Exception as err:
+        return jsonify({'Improper JSON data received': 'must follow the below format',
+            'Proper Data Format': {
+        'first_name': '<STRING>',
+        'last_name': '<STRING>',
+        'team': '<STRING>',
+        'pos': '<STRING> Midfielder | Defender | Keeper | Striker',
+        'injured': '<OPTIONAL BOOLEAN>',
+        'suspended': '<OPTIONAL BOOLEAN>'
+    }}), 406
+    try:
+        # then we need to upload it to our database
+        db.session.add(new_player)
+        db.session.commit()
+        return jsonify({'Created': new_player.to_dict()}), 201
+    # if there is a problem, send an error message
+    except:
+        return jsonify({'Create Player Rejected': 'A Player with that name already exists'}), 400
